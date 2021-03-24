35f99885d8166b044e5369af7ac86fc5dcba9543
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-24 12:19:53 +0100

    refactor: engloba tests en un describe

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,26 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('muestra una frase', () => {
-  render(<App />);
-  const quoteElement = screen.getByText(/Eighty percent of success is showing up./i);
-  expect(quoteElement).toBeInTheDocument();
-});
 
-test('muestra el autor de la frase', () => {
-  render(<App />);
-  const authorElement = screen.getByText(/Woody Allen/i);
-  expect(authorElement).toBeInTheDocument();
-});
+describe("muestra los elementos de Random Quote Machine", () => {
+  it('muestra una frase', () => {
+    render(<App />);
+    const quoteElement = screen.getByText(/Eighty percent of success is showing up./i);
+    expect(quoteElement).toBeInTheDocument();
+  });
 
-test('muestra un "boton" con la frase New quote', () => {
-  render(<App />);
-  const newQuoteButton = screen.getByRole('button', { name: /New Quote/i});
-  expect(newQuoteButton).toBeInTheDocument();
-});
+  it('muestra el autor de la frase', () => {
+    render(<App />);
+    const authorElement = screen.getByText(/Woody Allen/i);
+    expect(authorElement).toBeInTheDocument();
+  });
 
-test('muestra un elemento con titulo Tweet this quote', () => {
-  render(<App />);
-  const tweetElement = screen.getByTitle(/Tweet this quote!/i);
-  expect(tweetElement).toBeInTheDocument();
-});
+  it('muestra un "boton" con la frase New quote', () => {
+    render(<App />);
+    const newQuoteButton = screen.getByRole('button', { name: /New Quote/i});
+    expect(newQuoteButton).toBeInTheDocument();
+  });
+
+  it('muestra un elemento con titulo Tweet this quote', () => {
+    render(<App />);
+    const tweetElement = screen.getByTitle(/Tweet this quote!/i);
+    expect(tweetElement).toBeInTheDocument();
+  });
+})
