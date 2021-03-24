7ec0daace2a3d952e924783722479af407b7ceaf
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-24 10:34:10 +0100

    inital commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('muestra hola mundo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hola, mundo/i);
   expect(linkElement).toBeInTheDocument();
 });
