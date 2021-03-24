efd6d01f1be05441f20b37ce5c74e4235bb539f0
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-24 12:51:26 +0100

    refactor: quote pasa a ser un array

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 
 function App() {
 
-  const [quote,setQuote] = useState('Eighty percent of success is showing up.')
-  const [author,setAuthor] = useState('Woody Allen')
+  const [quote,setQuote] = useState(['Eighty percent of success is showing up.','Woody Allen'])
 
   const getNewQuote = () => {
-    setQuote("Winning isn’t everything, but wanting to win is.")
-    setAuthor("Vince lombardi")
+    setQuote(["Winning isn’t everything, but wanting to win is.","Vince lombardi"])
   }
 
   return (
     <>
-      <p>{quote}</p>
-      <p>{author}</p>
+      <p>{quote[0]}</p>
+      <p>{quote[1]}</p>
       <button onClick={getNewQuote}>New Quote</button>
       <a title="Tweet this quote!"></a>
     </>
