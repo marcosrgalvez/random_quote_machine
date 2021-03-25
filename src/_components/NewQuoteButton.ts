0081792ac4106ec00143587339bbfe62ec3116e6
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-25 12:34:46 +0100

    feat: maqueta boton

--- src/_components/NewQuoteButton.ts
+++ src/_components/NewQuoteButton.ts
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 
-export const NewQuoteButton = styled.button``;
+export const NewQuoteButton = styled.button`
+  margin-top: 1rem;
+  padding: 0.7rem;
+  border-radius: 0.25rem;
+
+  cursor: pointer;
+
+  font-size: 0.85rem;
+  color: white;
+  background-color: rgb(231, 76, 60);
+
+  :hover {
+    transition: opacity 150ms linear;
+    opacity: 0.9;
+  }
+`;
 
 export const ButtonWrapper = styled.div`
   display: flex;
