3216457984d393b53930cc05246d5e5e702b940d
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-29 12:02:52 +0200

    fix: corregido cambio color de fondo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
-import { createGlobalStyle } from "styled-components";
 import { Wrapper } from "./_components/styles//Wrapper";
 import QuoteBox from "./_components/QuoteBox";
 import { getColor } from "./services/colorService";
 
 function App(this: any) {
   const [color, setColor] = useState(getColor(""));
-
-  const BodyBgColor = createGlobalStyle`
-    body {
-      background-color: ${color}
-      
-    }`;
+  document.body.style.backgroundColor = color;
 
   return (
     <Wrapper>
       <QuoteBox color={color} setColor={setColor} />
-      <BodyBgColor />
     </Wrapper>
   );
 }
