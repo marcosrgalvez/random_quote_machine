82e1ede00ee60820a252936ab95e8cbe4a590db1
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-29 12:04:02 +0200

    refactor: extrae colores al servicio

--- src/services/colorService.ts
+++ src/services/colorService.ts
@@ -1,4 +1,17 @@
-import { colors } from "../ui/GlobalStyles/theme";
+const colors = [
+  "#16A085",
+  "#27AE60",
+  "#2C3E50",
+  "#F39C12",
+  "#E74C3C",
+  "#9B59B6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
 
 export const getColor = (oldColor: string) => {
   let mewColor = colors[Math.floor(Math.random() * colors.length)];
